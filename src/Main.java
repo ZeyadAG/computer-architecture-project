555baabd5031b7505fde82f5ffb6bbca6860e7d8
Zeyad Abdelghaffar <zeyadabdelghaffar@gmail.com>
2023-05-26 06:21:12 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Computer computer = new Computer();
+
+        computer.dataMemory.populateWithRandomValues();
+        computer.runProgram("programs/program_1.txt");
+
+    }
+}
